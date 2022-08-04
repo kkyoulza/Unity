8d816f8f3ab8d81b38c05e864cd46069aea71ab8
kkyoulza <66288087+kkyoulza@users.noreply.github.com>
2022-08-04 19:17:35 +0900

    Create MousePointer.cs
    Scene Manager에 적용된 스크립트입니다.

---
+++ 2D/MousePointer.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePointer : MonoBehaviour
+{
+    public GameObject pointerPrefab;
+    private GameObject pointerRed;
+    Vector2 mousePos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pointerRed = Instantiate(pointerPrefab) as GameObject;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = Input.mousePosition;
+        mousePos = UnityEngine.Camera.main.ScreenToWorldPoint(mousePos);
+        pointerRed.transform.position = mousePos;
+        Ray2D ray = new Ray2D(mousePos, Vector2.zero); // 원점 ~ 포인터로 발사되는 레이저
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log(mousePos);
+
+            float distance = Mathf.Infinity; // Ray 내에서 감지할 최대 거리
+
+            // RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, distance); // 다 잡음 
+            RaycastHit2D hitDrawer = Physics2D.Raycast(ray.origin, ray.direction, distance, 2048); // drawer 레이어만 잡음
+
+            if (hitDrawer)
+            {
+                Debug.Log("터치!");
+                Debug.Log(1 << LayerMask.NameToLayer("Touchable"));
+                Debug.Log(LayerMask.NameToLayer("Touchable"));
+            }
+
+        }
+
+    }
+}
