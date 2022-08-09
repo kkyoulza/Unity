c586366b03f4529a074321a0ef94fd2291efedd1
kkyoulza <66288087+kkyoulza@users.noreply.github.com>
2022-08-09 19:28:52 +0900

    Update ScoreManager.cs
    220809 - 테스트용 스폰 버튼에서 마이너스 점수를 주는 폭탄도 25%의 확률로 생성하게 하였음

--- 2D/ScoreManager.cs
+++ 2D/ScoreManager.cs
@@ -8,9 +8,13 @@ public class ScoreManager : MonoBehaviour
 {
     int TargetNum = 0; // 맞춰진 타겟의 종류!
     int cntScore;
+    int Rand_Spawn;
     public Text Score;
     float RandomFloatX,RandomFloatY;
-    public GameObject TargetPrefab;
+    public GameObject TargetPrefab1;
+    public GameObject TargetPrefab2;
+    public GameObject TargetPrefab3;
+    public GameObject Bomb1;
     private GameObject Target;
 
 
@@ -26,6 +30,15 @@ public class ScoreManager : MonoBehaviour
 
         switch (TargetNum)
         {
+            case -1:
+                cntScore = int.Parse(Score.text);
+                if (cntScore > 0)
+                {
+                    cntScore -= 1;
+                    Score.text = cntScore.ToString();
+                }
+                TargetNum = 0;
+                break;
             case 1:
                 cntScore = int.Parse(Score.text);
                 cntScore += 1;
@@ -52,15 +65,29 @@ public class ScoreManager : MonoBehaviour
         TargetNum = 2;
     }
 
+    public void MinusOne()
+    {
+        TargetNum = -1;
+    }
+
     public void SpawnTarget()
     {
+        Rand_Spawn = UnityEngine.Random.Range(0,3);
         UnityEngine.Random.InitState(DateTime.Now.Millisecond);
         RandomFloatX = UnityEngine.Random.Range(-8.2f, 8.4f);
         RandomFloatY = UnityEngine.Random.Range(-4.4f, 4.4f);
 
-        Target = Instantiate(TargetPrefab, new Vector2(RandomFloatX, RandomFloatY), Quaternion.identity) as GameObject;
+        if(Rand_Spawn >= 0 && Rand_Spawn < 2)
+        {
+            Target = Instantiate(TargetPrefab1, new Vector2(RandomFloatX, RandomFloatY), Quaternion.identity) as GameObject;
+        }
+        else if(Rand_Spawn == 2)
+        {
+            Target = Instantiate(Bomb1, new Vector2(RandomFloatX, RandomFloatY), Quaternion.identity) as GameObject;
+        }
 
-        if(RandomFloatX > 0)
+
+        if (RandomFloatX > 0)
             Target.GetComponent<Rigidbody2D>().AddForce(new Vector2(-500, 0));
         else
             Target.GetComponent<Rigidbody2D>().AddForce(new Vector2(500, 0));
