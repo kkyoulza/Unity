03fac88da339ced5a1bea37e20e2c128c5eb0f52
kkyoulza <66288087+kkyoulza@users.noreply.github.com>
2022-08-10 19:23:02 +0900

    Update BulletManager.cs
    220810 - 총알 개수만큼 그림 갱신기능 추가 + 0발에서만 10발 충전이 가능하게 제한 추가

--- 2D/BulletManager.cs
+++ 2D/BulletManager.cs
@@ -7,6 +7,11 @@ public class BulletManager : MonoBehaviour
 {
     int remainedBullet = 0;
     public Text bulletText;
+    List<string> BulletImgName = new List<string>();
+    public Image BulletImg;
+    
+    public GameObject UIBase;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -22,17 +27,47 @@ public class BulletManager : MonoBehaviour
 
     public void AddBullet(int count)
     {
-        this.remainedBullet = count;
+        if(remainedBullet == 0)
+        {
+            BulletImgName.Clear();
+            this.remainedBullet = count;
+            SetBulletUI();
+        }
+        else
+        {
+            Debug.Log("총알이 다 떨어졌을 때 충전할 수 있습니다.");
+
+        }
+        
     }
 
     public void discountBullet(int count)
     {
-        this.remainedBullet -= count;
+        this.remainedBullet -= count; // 총알 소비
+
+        Destroy(GameObject.Find(BulletImgName[remainedBullet])); // 총알을 한 발 까고 나면 리스트에서 가장 밖에 있는 원소의 위치에 갈 수 있다.
+
     }
 
     public int GetBulletCount()
     {
         return this.remainedBullet;
+    }
+
+    public void SetBulletUI()
+    {
+        for(int i = 0; i < remainedBullet; i++)
+        {
+            Vector3 offSet = UIBase.transform.position + new Vector3(340 - i * 50, -160, 0); // 나중에 생성된 총알일수록 안쪽에 생성되게끔!
+
+            Image BulletImsi = Instantiate(BulletImg);
+            BulletImsi.transform.SetParent(UIBase.transform,false); // UI 팔레트를 부모 객체로 설정해 놓는다.
+            BulletImsi.name = "BulImge" + i;
+            BulletImsi.transform.position = offSet;
+
+            BulletImgName.Add(BulletImsi.name); // 이름을 저장 해 놨다가 역순으로 Destroy 해 준다.(총알 소비 시)
+
+        }
     }
 
 }
