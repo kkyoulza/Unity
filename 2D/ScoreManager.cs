c773aa3b4f30df6db311b70b5a788177e404c1e8
kkyoulza <66288087+kkyoulza@users.noreply.github.com>
2022-08-12 17:45:54 +0900

    Update ScoreManager.cs
    220812 - spawn 부분 분리

--- 2D/ScoreManager.cs
+++ 2D/ScoreManager.cs
@@ -8,15 +8,8 @@ public class ScoreManager : MonoBehaviour
 {
     int TargetNum = 0; // 맞춰진 타겟의 종류!
     int cntScore;
-    int Rand_Spawn;
-    public Text Score;
-    float RandomFloatX,RandomFloatY;
-    public GameObject TargetPrefab1;
-    public GameObject TargetPrefab2;
-    public GameObject TargetPrefab3;
-    public GameObject Bomb1;
-    private GameObject Target;
 
+    public Text Score;
 
     // Start is called before the first frame update
     void Start()
@@ -70,27 +63,6 @@ public class ScoreManager : MonoBehaviour
         TargetNum = -1;
     }
 
-    public void SpawnTarget()
-    {
-        Rand_Spawn = UnityEngine.Random.Range(0,3);
-        UnityEngine.Random.InitState(DateTime.Now.Millisecond);
-        RandomFloatX = UnityEngine.Random.Range(-8.2f, 8.4f);
-        RandomFloatY = UnityEngine.Random.Range(-4.4f, 4.4f);
-
-        if(Rand_Spawn >= 0 && Rand_Spawn < 2)
-        {
-            Target = Instantiate(TargetPrefab1, new Vector2(RandomFloatX, RandomFloatY), Quaternion.identity) as GameObject;
-        }
-        else if(Rand_Spawn == 2)
-        {
-            Target = Instantiate(Bomb1, new Vector2(RandomFloatX, RandomFloatY), Quaternion.identity) as GameObject;
-        }
-
-
-        if (RandomFloatX > 0)
-            Target.GetComponent<Rigidbody2D>().AddForce(new Vector2(-500, 0));
-        else
-            Target.GetComponent<Rigidbody2D>().AddForce(new Vector2(500, 0));
-    }
+    
 
 }
