84c063222eb9d82d6b7581758b57c9eaf76724b3
kkyoulza <66288087+kkyoulza@users.noreply.github.com>
2022-08-15 18:34:16 +0900

    Update StageManager.cs
    220815 - MinusTarget1()에서 타겟 감소 후에 총알 개수가 감소되게 하였다. (대상을 맞췄을 때!)

--- 2D/StageManager.cs
+++ 2D/StageManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System;
+using UnityEngine.SceneManagement; // Scene을 관리하기 위해!
 
 public class StageManager : MonoBehaviour
 {
@@ -18,20 +19,32 @@ public class StageManager : MonoBehaviour
     private List<String> Target1Name = new List<String>();
     private List<String> Bomb1Name = new List<String>();
 
+    public GameObject Clear;
+    public Text ScoreTxt; // 클리어시 보여줄 스코어 텍스트
+    public GameObject GameOver;
+
+    public Text Score; // 우상단에 있는 스코어 텍스트
+
     int remainTarget1 = -1; // 남은 타겟의 수를 실시간으로 갱신해 주기 위한 것, 이 것이 0이 되면 다음 스테이지로 간다.
     // -1 > 체크를 하지 않는 상태
 
+    BulletManager bullet;
 
     // Start is called before the first frame update
     void Start()
     {
+        bullet = GetComponent<BulletManager>(); // bullet Manager를 가져온다.
         Stage1();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         Stage1Check();
+        GameOverCheck();
+
     }
 
     public void SpawnTarget1(int num)
@@ -71,9 +84,10 @@ public class StageManager : MonoBehaviour
     public void Stage1()
     {
         remainTarget1 = 10;
-        for(int i = 0; i < 10; i++)
+        GetComponent<BulletManager>().AddBullet(15);
+        for(int i = 0; i < remainTarget1; i++)
         {
-            SpawnTarget1(i); // 타겟 10개 소환
+            SpawnTarget1(i); // 타겟 10개 소환 
             Debug.Log("타겟" + i + "개 소환");
             if (i < 5) // 폭탄 5개 소환
                 SpawnBomb1(i);
@@ -81,11 +95,66 @@ public class StageManager : MonoBehaviour
 
     }
 
+    public void GameOverCheck()
+    {
+        if(remainTarget1 > 0 && (GetComponent<BulletManager>().GetBulletCount() == 0))
+        {
+            //타겟이 남았을 때 총알을 다 소비하였다면..!
+            Debug.Log("총알을 다 소비하였습니다! 게임 오버!");
+
+            GameOver.SetActive(true);
+
+            for (int i = 0; i < Target1Name.Count; i++)
+            {
+                try
+                {
+                    Destroy(GameObject.Find(Target1Name[i]));
+                }
+                catch
+                {
+                    Debug.Log("맞춘 타겟이 있어서 스킵!");
+                    continue;
+                }
+
+            }
+
+            for (int i = 0; i < Bomb1Name.Count; i++)
+            {
+                try
+                {
+                    Destroy(GameObject.Find(Bomb1Name[i]));
+                }
+                catch
+                {
+                    Debug.Log("이미 맞춘 폭탄이 있어서 다음 것을 제거합니다!");
+                    continue;
+                }
+
+            }
+            Bomb1Name.Clear();
+            Target1Name.Clear();
+
+            remainTarget1 = -1; // checkOff상태로 변경!
+
+
+            Invoke("BackToTheMap", 2); // 2초후 BackToTheMap 호출
+
+
+
+        }
+    }
+
     public void Stage1Check()
     {
         if(remainTarget1 == 0)
         {
             Debug.Log("스테이지 1 클리어! 폭탄을 제거합니다.");
+
+            Clear.SetActive(true);
+            ScoreTxt.text = (int.Parse(Score.text) + GetComponent<BulletManager>().GetBulletCount() * 10).ToString();
+            // 타겟 스코어 + 남은 총알 개수 * 10
+
+
             for(int i = 0; i < Bomb1Name.Count; i++)
             {
                 try
@@ -109,6 +178,12 @@ public class StageManager : MonoBehaviour
     public void MinusTarget1()
     {
         remainTarget1--;
+        bullet.discountBullet(); // 맞췄을 때 총알 감소
+    }
+
+    public void BackToTheMap()
+    {
+        SceneManager.LoadScene("MainMap");
     }
 
 }
